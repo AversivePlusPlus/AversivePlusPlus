3a487122de78116a219544682fe125954adcb23f
astralien3000 <astralien3000@yahoo.fr>
2016-01-07 17:10:25 +0100

    Improved STM32 cmake toolchain

--- cmake/toolchain/generic.cmake
+++ cmake/toolchain/generic.cmake
@@ -2,8 +2,8 @@ cmake_minimum_required(VERSION 2.6)
 
 set(GENERIC_FLAGS "-Wall -Wextra")
 
-set(CMAKE_C_FLAGS_INIT   "-std=c11 ${GENERIC_FLAGS}")
-set(CMAKE_CXX_FLAGS_INIT "-std=c++11 ${GENERIC_FLAGS}")
-set(CMAKE_ASM_FLAGS_INIT "${GENERIC_FLAGS}")
+set(CMAKE_C_FLAGS   "-std=c11 ${GENERIC_FLAGS}")
+set(CMAKE_CXX_FLAGS "-std=c++11 ${GENERIC_FLAGS}")
+set(CMAKE_ASM_FLAGS "${GENERIC_FLAGS}")
 
 unset(GENERIC_FLAGS)
