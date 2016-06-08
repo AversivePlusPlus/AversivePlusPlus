f3e70dcf649bdddfde8e0ed4640eeffe37db90ac
Loïc Dauphin <astralien3000@yahoo.fr>
2016-06-08 12:06:14 +0200

    All modules under conan

--- toolchain/generic.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 2.8)
-
-set(GENERIC_FLAGS "-Wall -Wextra")
-
-set(CMAKE_C_FLAGS   "-std=c11 ${GENERIC_FLAGS}")
-set(CMAKE_CXX_FLAGS "-std=c++11 ${GENERIC_FLAGS}")
-set(CMAKE_ASM_FLAGS "${GENERIC_FLAGS}")
-
-unset(GENERIC_FLAGS)
