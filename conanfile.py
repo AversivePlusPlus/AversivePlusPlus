bc9645291294e28960b56247e48de4f5ed11dd7f
Loïc Dauphin <astralien3000@yahoo.fr>
2016-06-17 16:28:00 +0200

    Added arduino modules. Debug some others modules.

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,10 @@ class AversivePlusPlusModuleConan(ConanFile):
     version = "0.1"
     exports = "Makefile"
     settings = "target"
-    requires = "base/0.1@AversivePlusPlus/stable", "device-2wheel/0.1@AversivePlusPlus/stable", "feetech/0.1@AversivePlusPlus/stable", "hal-switch/0.1@AversivePlusPlus/stable", "memory_mapping/0.1@AversivePlusPlus/stable", "stream/0.1@AversivePlusPlus/stable", "cas/0.1@AversivePlusPlus/stable", "container/0.1@AversivePlusPlus/stable", "device-control/0.1@AversivePlusPlus/stable", "filter/0.1@AversivePlusPlus/stable", "stream-hal/0.1@AversivePlusPlus/stable", "device/0.1@AversivePlusPlus/stable", "device-hal/0.1@AversivePlusPlus/stable", "hal/0.1@AversivePlusPlus/stable", "ik/0.1@AversivePlusPlus/stable", "toolchain-switch/0.1@AversivePlusPlus/stable"
+    requires = "base/0.1@AversivePlusPlus/dev", "device-2wheel/0.1@AversivePlusPlus/dev", "feetech/0.1@AversivePlusPlus/dev", "hal-switch/0.1@AversivePlusPlus/dev", "memory_mapping/0.1@AversivePlusPlus/dev", "stream/0.1@AversivePlusPlus/dev", "cas/0.1@AversivePlusPlus/dev", "container/0.1@AversivePlusPlus/dev", "device-control/0.1@AversivePlusPlus/dev", "filter/0.1@AversivePlusPlus/dev", "stream-hal/0.1@AversivePlusPlus/dev", "device/0.1@AversivePlusPlus/dev", "device-hal/0.1@AversivePlusPlus/dev", "hal/0.1@AversivePlusPlus/dev", "ik/0.1@AversivePlusPlus/dev", "toolchain-switch/0.1@AversivePlusPlus/dev"
 
     def config(self):
-        if self.settings.target == "sasiae":
-            self.requires("sasiae/0.1@AversivePlusPlus/stable")
+        #if self.settings.target == "sasiae":
+        #    self.requires("sasiae/0.1@AversivePlusPlus/dev")
+        if self.settings.target == "board" and self.settings.target.board in ["Arduino Uno", "Arduino Mega"]:
+            self.requires("arduino/0.1@AversivePlusPlus/dev")
