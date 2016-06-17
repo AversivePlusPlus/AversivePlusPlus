5d1194b452f1f45b787f7919c35bd198ed807040
Loïc Dauphin <astralien3000@yahoo.fr>
2016-06-17 12:24:36 +0200

    Added conan launcher. Debug conanfile

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 class AversivePlusPlusModuleConan(ConanFile):
     name = "AversivePlusPlus"
     version = "0.1"
-    exports = ""
+    exports = "Makefile"
     settings = "target"
     requires = "base/0.1@AversivePlusPlus/stable", "device-2wheel/0.1@AversivePlusPlus/stable", "feetech/0.1@AversivePlusPlus/stable", "hal-switch/0.1@AversivePlusPlus/stable", "memory_mapping/0.1@AversivePlusPlus/stable", "stream/0.1@AversivePlusPlus/stable", "cas/0.1@AversivePlusPlus/stable", "container/0.1@AversivePlusPlus/stable", "device-control/0.1@AversivePlusPlus/stable", "filter/0.1@AversivePlusPlus/stable", "stream-hal/0.1@AversivePlusPlus/stable", "device/0.1@AversivePlusPlus/stable", "device-hal/0.1@AversivePlusPlus/stable", "hal/0.1@AversivePlusPlus/stable", "ik/0.1@AversivePlusPlus/stable", "toolchain-switch/0.1@AversivePlusPlus/stable"
 
