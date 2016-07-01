c4d9b1bdcf1ad5dd3d5353bca3ceb9f58029f545
Loïc Dauphin <astralien3000@yahoo.fr>
2016-07-01 17:39:13 +0200

    Update conan

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, Embedded
 
 class AversivePlusPlusModuleConan(ConanFile):
     name = "AversivePlusPlus"
@@ -8,7 +8,7 @@ class AversivePlusPlusModuleConan(ConanFile):
     requires = "base/0.1@AversivePlusPlus/dev", "device-2wheel/0.1@AversivePlusPlus/dev", "feetech/0.1@AversivePlusPlus/dev", "hal-switch/0.1@AversivePlusPlus/dev", "memory_mapping/0.1@AversivePlusPlus/dev", "stream/0.1@AversivePlusPlus/dev", "cas/0.1@AversivePlusPlus/dev", "container/0.1@AversivePlusPlus/dev", "device-control/0.1@AversivePlusPlus/dev", "filter/0.1@AversivePlusPlus/dev", "stream-hal/0.1@AversivePlusPlus/dev", "device/0.1@AversivePlusPlus/dev", "device-hal/0.1@AversivePlusPlus/dev", "hal/0.1@AversivePlusPlus/dev", "ik/0.1@AversivePlusPlus/dev", "toolchain-switch/0.1@AversivePlusPlus/dev"
 
     def config(self):
-        #if self.settings.target == "sasiae":
-        #    self.requires("sasiae/0.1@AversivePlusPlus/dev")
-        if self.settings.target == "board" and self.settings.target.board in ["Arduino Uno", "Arduino Mega"]:
+        embed = Embedded(self.settings)
+        if self.settings.target == "board" and self.settings.target.board in ["arduino_uno", "arduino_mega"]:
             self.requires("arduino/0.1@AversivePlusPlus/dev")
+
